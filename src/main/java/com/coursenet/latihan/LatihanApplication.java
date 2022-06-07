88efa2b4593fa5c5b0f17c470ed249b91bcba5ad
audiprtm <audiprtm@gmail.com>
2022-06-07 20:48:57 +0700

    test branch

--- src/main/java/com/coursenet/latihan/LatihanApplication.java
+++ src/main/java/com/coursenet/latihan/LatihanApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//Test comment
 public class LatihanApplication {
 
 	public static void main(String[] args) {
