01ec90555b75b82d0e748b2e6ea29ca7d6371790
audiprtm <audiprtm@gmail.com>
2022-06-07 19:54:40 +0700

    revert commit

--- src/main/java/com/coursenet/latihan/LatihanApplication.java
+++ src/main/java/com/coursenet/latihan/LatihanApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//Testing commit 2
 public class LatihanApplication {
 
 	public static void main(String[] args) {
