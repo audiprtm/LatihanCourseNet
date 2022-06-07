20cd1571dc1f4c82e1527e5463f2a92b8bc702a0
audiprtm <audiprtm@gmail.com>
2022-06-07 19:37:53 +0700

    commit 2

--- src/main/java/com/coursenet/latihan/LatihanApplication.java
+++ src/main/java/com/coursenet/latihan/LatihanApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//Testing commit
+//Testing commit 2
 public class LatihanApplication {
 
 	public static void main(String[] args) {
