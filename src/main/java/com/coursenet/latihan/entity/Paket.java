e2eef832b48e3dcef1cf3717bed8bb55b2828a3f
audiprtm <audiprtm@gmail.com>
2022-06-09 21:46:20 +0700

    hapus commit

--- src/main/java/com/coursenet/latihan/entity/Paket.java
+++ src/main/java/com/coursenet/latihan/entity/Paket.java
@@ -17,7 +17,6 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 public class Paket {
-	//id, namaBarang, createAt,updatedAt
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
